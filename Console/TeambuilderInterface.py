4eca7f39e58609d75d15ca18964068cbcd6ed9f7
Darmogus <dark.dario06@gmail.com>
2024-03-17 23:44:00 +0100

    CTk

--- Console/TeambuilderInterface.py
+++ Console/TeambuilderInterface.py
@@ -1,6 +1,6 @@
 from tkinter import *
-from tkinter import ttk
-from PIL import Image, ImageTk
+from tkinter import Label, PhotoImage
+from PIL import Image
 import customtkinter
 
 from PokemonListConsole import AVAILABLE_POKEMONS
@@ -19,89 +19,52 @@ class TeambuilderInterface:
         self.mainframe = customtkinter.CTkFrame(self.master)
         self.mainframe.pack(fill=BOTH, expand=True)
 
+        self.tabs = customtkinter.CTkTabview(self.mainframe, height=750, width=500, corner_radius=20)
+        self.tabs.pack(pady=10)
+        self.tabs_objects = [PokemonTab(self.tabs, i) for i in range(1, 7)]
+
         self.create_tabs()
 
     def create_tabs(self):
-        self.tabs = customtkinter.CTkTabview(self.mainframe, height=750, width=500)
-        self.tabs.pack(pady=10)
-
-        for i in range(1, 7):
-            PokemonTab(self.tabs, i)
-
-        # Make the tabs take the entire space in width
-        self.mainframe.columnconfigure(0, weight=1)
-        self.mainframe.rowconfigure(0, weight=2)  # Increase the weight to make the tabs a little taller
+        pass
 
 
 class PokemonTab:
-    def __init__(self, master, column: int):
+    def __init__(self, master, index: int):
         self.master = master
-        self.column = column
-        self.tab = customtkinter.CTkFrame(self.master)
-        self.master.add(f"  Pokemon {column}  ")
+        self.column = index
+        self.tab = self.master.add(f"  Pokemon {index}  ")
 
-        # Creating the frames
-        self.image_frame = customtkinter.CTkFrame(self.tab)
-        self.utilities_frame = customtkinter.CTkFrame(self.tab)
-        self.moves_frame = customtkinter.CTkFrame(self.tab)
-        self.stats_frame = customtkinter.CTkFrame(self.tab)
+        self.tab.grid_rowconfigure(0, weight=1)
+        self.tab.grid_rowconfigure(1, weight=1)
 
-        # Séparators
-        self.separator1 = ttk.Separator(self.tab, orient=VERTICAL)
-        self.separator2 = ttk.Separator(self.tab, orient=VERTICAL)
-        self.separator3 = ttk.Separator(self.tab, orient=HORIZONTAL)
+        self.tab.grid_columnconfigure(0, weight=1)
 
-        self.image_frame.grid(row=0, column=0, sticky="nsew")
-        self.separator1.grid(row=0, column=1, sticky="ns")
-        self.utilities_frame.grid(row=0, column=2, sticky="nsew")
-        self.separator2.grid(row=0, column=3, sticky="ns")
-        self.moves_frame.grid(row=0, column=4, sticky="nsew")
-        self.separator3.grid(row=1, column=0, columnspan=5, sticky="ew")
-        self.stats_frame.grid(row=2, column=0, columnspan=5, sticky="nsew")
+        self.upper_frame = customtkinter.CTkFrame(self.tab, corner_radius=20)
+        self.upper_frame.grid(row=0, column=0, sticky="nsew")
+        self.upper_frame.grid_propagate(False)  # Prevent resizing based on contents
 
-        # Set the weights for the rows and columns
-        self.tab.rowconfigure(0, weight=1)
-        self.tab.rowconfigure(2, weight=3)
-        self.tab.columnconfigure(0, weight=0)
-        self.tab.columnconfigure(1, weight=0)  # Pas de poids pour le séparateur
-        self.tab.columnconfigure(2, weight=1)
-        self.tab.columnconfigure(3, weight=0)  # Pas de poids pour le séparateur
-        self.tab.columnconfigure(4, weight=1)
+        self.lower_frame = customtkinter.CTkFrame(self.tab, corner_radius=20)
+        self.lower_frame.grid(row=1, column=0, sticky="nsew")
+        self.lower_frame.grid_propagate(False)
 
-        self.utilities_frame.grid_propagate(False)
-        self.moves_frame.grid_propagate(False)
+        self.my_image = PhotoImage(file="../Images/pixel-art-pokeball.png")
+
+        self.my_label = Label(self.upper_frame, image=self.my_image)
+        self.my_label.image = self.my_image
+        self.my_label.grid(row=0, column=0)
+
 
         # Create a canvas to display the pokemon
-        self.canvas = customtkinter.CTkCanvas(self.image_frame, width=200, height=150)
-        self.canvas.grid(row=0, column=0, sticky="N, W, E, S")
-        image = ImageTk.PhotoImage(Image.open(f"../Images/pixel-art-pokeball.png"))
-        self.img_item = self.canvas.create_image(50, 75, anchor=W, image=image)
-        self.canvas.image = image
-
-        # Create a dropdown menu to select a pokemon
-        self.var = StringVar(self.image_frame)
-        self.var.set("Select a Pokemon")
-        self.var.trace("w", self.on_pokemon_selected)
-
-        options = ttk.Combobox(self.image_frame, textvariable=self.var, values=[pokemon.name for pokemon in AVAILABLE_POKEMONS], width=30, state="readonly")
-        options.grid(row=1, column=0)
-
-
-
-
-
-    def on_pokemon_selected(self, *args):
-        selected_pokemon = self.var.get()
-        if selected_pokemon != "Select a Pokemon":
-            image = ImageTk.PhotoImage(Image.open(f"../Images/Sprites/{selected_pokemon.lower().replace(' ', '-')}.gif"))
-            self.canvas.delete(self.img_item)
-            self.img_item = self.canvas.create_image(100, 75, anchor=CENTER, image=image)
-            self.canvas.image = image
-
+        # self.canvas = customtkinter.CTkCanvas(self.upper_frame, width=200, height=150)
+        #self.canvas.grid(row=0, column=0, sticky="N, W, E, S")
+        #image = ImageTk.PhotoImage(Image.open(f"../Images/pixel-art-pokeball.png"))
+        #self.img_item = self.canvas.create_image(50, 75, anchor=W, image=image)
+        #self.canvas.image = image
 
 def main():
     root = customtkinter.CTk()
-    app = TeambuilderInterface(root)
+    TeambuilderInterface(root)
     root.mainloop()
 
 
--- Console/Tests.py
+++ Console/Tests.py
@@ -1,20 +1,13 @@
-import tkinter as tk
+import customtkinter
+from PIL import Image
 
-# Création de la fenêtre principale
-root = tk.Tk()
-root.title("Séparateurs avec grid")
 
-# Séparateur horizontal
-separator_horizontal = tk.Frame(root, height=2, relief='sunken', bg='black')
-separator_horizontal.grid(row=1, column=0, columnspan=2, sticky='ew', padx=10, pady=5)
+root = customtkinter.CTk()
 
-# Texte entre les séparateurs
-label = tk.Label(root, text="Contenu entre les séparateurs")
-label.grid(row=0, column=0, columnspan=2, padx=10, pady=5)
 
-# Séparateur vertical
-separator_vertical = tk.Frame(root, width=2, relief='sunken', bg='black')
-separator_vertical.grid(row=0, column=1, rowspan=2, sticky='ns', padx=5, pady=10)
+my_image = customtkinter.CTkImage(dark_image=Image.open("../Images/pixel-art-pokeball.png"), light_image=Image.open("../Images/pixel-art-pokeball.png"), size=(100, 100))
 
-# Lancement de la boucle principale
+my_label = customtkinter.CTkLabel(root, text="", image=my_image)
+my_label.pack(pady=10)
+
 root.mainloop()
